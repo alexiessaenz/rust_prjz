5665e06c724d30b84e622b97806050266e9e98b2
alexiessaenz <00400815@uca.edu.sv>
2024-09-11 00:44:53 -0600

    feat[structs]: structs and enums

--- structs_and_enums/target/debug/deps/structs_and_enums-c0e09c14f65f9505.d
+++
@@ -1,5 +0,0 @@
-C:\workspace5\rust_prjz\structs_and_enums\target\debug\deps\libstructs_and_enums-c0e09c14f65f9505.rmeta: src/main.rs
-
-C:\workspace5\rust_prjz\structs_and_enums\target\debug\deps\structs_and_enums-c0e09c14f65f9505.d: src/main.rs
-
-src/main.rs:
--- structs_and_enums/target/debug/structs_and_enums.d
+++
@@ -1,1 +0,0 @@
-C:\workspace5\rust_prjz\structs_and_enums\target\debug\structs_and_enums.exe: C:\workspace5\rust_prjz\structs_and_enums\src\main.rs
